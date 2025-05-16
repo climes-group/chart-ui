4fde3c6a6405e1a24f93546c130c5facdcc8da48
Michael Bieniek <michael.c.bieniek@gmail.com>
2025-05-16 18:19:21 -0400

    Create alias for ./src

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteBasicSslPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     // support `describe`, `test` etc. globally,
     // so you don't need to import them every time
